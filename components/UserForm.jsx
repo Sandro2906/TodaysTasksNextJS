6537afbd2c1746861660b4274daa89a2597eaa04
Sandro Gataric <132219558+Sandro2906@users.noreply.github.com>
2023-12-12 20:08:11 +0100

    Update UserForm.jsx

--- components/UserForm.jsx
+++ components/UserForm.jsx
@@ -6,8 +6,8 @@ import React from 'react'
 
 const UserForm = () => {
 const router = useRouter();
-    const [title,setTitle] = useState('')
-    const [task,setTask] = useState('')
+    const [title,setTitle] = useState("")
+    const [task,setTask] = useState("")
     
 
     
@@ -24,20 +24,20 @@ const router = useRouter();
             }),
           });
 
-          router.push('/getting')
+          router.push("/getting")
     }
 
   return (
-    <div className='w-full min-h-screen flex justify-center items-center flex-col'>
+    <div className="w-full min-h-screen flex justify-center items-center flex-col">
     <h1 className="text-3xl text-white font-bold">Today's Tasks</h1>
     
-    <form className='flex flex-col items-center' onSubmit={handleSubmit}>
-        <input placeholder='Title' className='text-black w-[300px] h-auto p-2 border-2 border-yellow-500 rounded-md' type='text' value={title} onChange={e => setTitle(e.target.value)}/>
-        <textarea rows={6} placeholder='Task' className='text-black w-[300px]  p-2 border-2 border-yellow-500 rounded-md my-2'  value={task} onChange={e => setTask(e.target.value)}/>
-        <button className='text-white bg-yellow-500 w-[130px] h-auto p-3 rounded-md' type='submit'>POST</button>
+    <form className="flex flex-col items-center" onSubmit={handleSubmit}>
+       <input placeholder="Title" className="text-black w-[300px] h-auto p-2 border-2 border-yellow-500 rounded-md" type="text" value={title} onChange={e => setTitle(e.target.value)}/>
+        <textarea rows={6} placeholder="Task" className="text-black w-[300px]  p-2 border-2 border-yellow-500 rounded-md my-2"  value={task} onChange={e => setTask(e.target.value)}/>
+        <button className="text-white bg-yellow-500 w-[130px] h-auto p-3 rounded-md" type="submit">POST</button>
     </form>
     </div>
   )
 }
 
-export default UserForm
+export default UserForm;
